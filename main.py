1e18133bbcf02c40b8f4b6748ce3618453c50823
angelo <gloria.aam@obmontessori.edu.ph>
2026-09-17 22:36:02 +0800

    3

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from pyscript import document, display
 
 def create_order(e):
-    document.getElementById("ST_output").innerHTML = ""
+    document.getElementById("ST_output").innerHTML = "" #reset to avoid the totals stacking in the receipt
+    document.getElementById("VAT_output").innerHTML = "" #reset
+    document.getElementById("TA_output").innerHTML = "" #reset
 
-    tea1 = document.getElementById("input1")
-    tea2 = document.getElementById("input2")
-    tea3 = document.getElementById("input3")
+    tea1 = document.getElementById("input1") #get value from cook's tea
+    tea2 = document.getElementById("input2") #get value from rock's tea
+    tea3 = document.getElementById("input3") #get value from monster's tea
 
-    subtotal1 = float(tea1.value) * tea1.checked
-    subtotal2 = float(tea2.value) * tea2.checked
-    subtotal3 = float(tea3.value) * tea3.checked
+    subtotal1 = float(tea1.value) * tea1.checked #to check if the checkbox is checked (checked = *1 and unchecked = *0)
+    subtotal2 = float(tea2.value) * tea2.checked 
+    subtotal3 = float(tea3.value) * tea3.checked 
+    
+    subtotal = subtotal1 + subtotal2 + subtotal3 #computation for the subtotal
+    vat = subtotal * 0.12 #computation for the tax
+    total_amount = subtotal + vat #computation for the total amount
 
-    subtotal = subtotal1 + subtotal2 + subtotal3
-    vat = subtotal * 0.12
-    total_amount = subtotal + vat
-
-    display(f'Receipt: <br>Subtotal: ${subtotal} <br>Tax: ${vat} <br>Total: ${total_amount}', target="ST_output")
+    display(f'Subtotal: ${subtotal}', target="ST_output") #diplays in the receipt 1
+    display(f'Tax: ${vat}', target="VAT_output") #displays in the receipt 2
+    display(f'Total: ${total_amount}', target="TA_output") #displays in the receipt 3
